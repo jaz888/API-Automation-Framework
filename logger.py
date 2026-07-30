a5f686b48df8afe33719117a532b0e7315c24f6d
Jazib <jazibirfan32@gmail.com>
2026-07-30 22:16:46 +0500

    Create logs directory before configuring logging

--- logger.py
+++ logger.py
@@ -1,9 +1,11 @@
 import logging
+import os
 
+os.makedirs("logs", exist_ok=True)
 logging.basicConfig(
     filename="logs/test.log",
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-logger = logging.getLogger()
+logger = logging.getLogger(__name__)
